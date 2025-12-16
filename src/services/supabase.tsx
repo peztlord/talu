b98461c74fed13a59453b28116f0671d873dd6e3
LferrazCoelho <lferraz.dev@gmail.com>
2025-12-16 16:28:57 -0300

    new version

--- src/services/supabase.tsx
+++ src/services/supabase.tsx
@@ -4,7 +4,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Faltam as variáveis de ambiente do Supabase');
+  throw new Error('Faltam as variáveis do Supabase');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
